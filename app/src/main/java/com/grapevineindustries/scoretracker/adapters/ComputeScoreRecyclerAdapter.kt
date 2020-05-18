a6cf42ee5e7d58006b4fd2eaf41384f48ba656ed
Nathan Welch <nawwx3@umsystem.edu>
2020-05-18 06:51:16 -0500

    names and scores updating correctly. so is wildcard

--- app/src/main/java/com/grapevineindustries/scoretracker/adapters/ComputeScoreRecyclerAdapter.kt
+++ app/src/main/java/com/grapevineindustries/scoretracker/adapters/ComputeScoreRecyclerAdapter.kt
@@ -4,12 +4,24 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.grapevineindustries.scoretracker.R
+import com.grapevineindustries.scoretracker.model.Player
 
-class ComputeScoreRecyclerAdapter(val context: Context, val players: String): RecyclerView.Adapter<ComputeScoreRecyclerAdapter.ComputeScoreHolder>() {
+class ComputeScoreRecyclerAdapter(val context: Context, val players: ArrayList<Player>): RecyclerView.Adapter<ComputeScoreRecyclerAdapter.ComputeScoreHolder>() {
 
-    inner class ComputeScoreHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    inner class ComputeScoreHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: TextView? = itemView.findViewById(R.id.tv_name)
+        val score: TextView? = itemView.findViewById(R.id.tv_score)
+        val addScore: EditText? = itemView.findViewById(R.id.et_score)
+
+        fun bindComputeScore(player: Player, context: Context) {
+            name?.text = player.name
+            score?.text = player.score.toString()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComputeScoreHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.compute_score_list_item, parent, false)
@@ -17,10 +29,11 @@ class ComputeScoreRecyclerAdapter(val context: Context, val players: String): Re
     }
 
     override fun getItemCount(): Int {
-        return Integer.valueOf(players)
+        return Integer.valueOf(players.count())
     }
 
     override fun onBindViewHolder(holder: ComputeScoreHolder, position: Int) {
         println("we got here")
+        holder.bindComputeScore(players[position], context)
     }
 }
--- app/src/main/java/com/grapevineindustries/scoretracker/controller/ComputeScoreActivity.kt
+++ app/src/main/java/com/grapevineindustries/scoretracker/controller/ComputeScoreActivity.kt
@@ -1,9 +1,11 @@
 package com.grapevineindustries.scoretracker.controller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.grapevineindustries.scoretracker.R
 import com.grapevineindustries.scoretracker.adapters.ComputeScoreRecyclerAdapter
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.compute_score_list_item.view.tv_score
 
 class ComputeScoreActivity : AppCompatActivity() {
 
+    var wildcard = 2  // needs to start at 2 because will increment before displaying
     lateinit var playerList: ArrayList<Player>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +28,13 @@ class ComputeScoreActivity : AppCompatActivity() {
         setContentView(R.layout.activity_compute_score)
 
         playerList = intent.getParcelableArrayListExtra(EXTRA_NUM_PLAYERS)
-        val adapter = ComputeScoreRecyclerAdapter(this, "5")
+        val adapter = ComputeScoreRecyclerAdapter(this, playerList)
 
         rv_computeScore.adapter = adapter
 
         val layoutManager = LinearLayoutManager(this)
         rv_computeScore.layoutManager = layoutManager
+        tv_wildCard.text = "${convertWildcard()}'s are wild"
     }
 
     fun onTallyScoreClicked(view: View) {
@@ -52,5 +56,26 @@ class ComputeScoreActivity : AppCompatActivity() {
             slot.tv_score.text = playerList[num].score.toString()
             slot.et_score.text.clear()
         }
+        updateWildcard()
+
+        if (wildcard == 14) {
+            val toast = Toast.makeText(this, "Game is over", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+    fun convertWildcard(): String {
+        wildcard++
+        return when(wildcard) {
+            11 -> "J"
+            12 -> "Q"
+            13 -> "K"
+            else -> wildcard.toString()
+        }
+    }
+
+    fun updateWildcard()
+    {
+        tv_wildCard.text = "${convertWildcard()}'s are wild"
     }
 }
