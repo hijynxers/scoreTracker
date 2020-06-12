b858f4318fc70d4eb6bc276d7c1ab6ebb49ca8ec
Nathan Welch <nawwx3@umsystem.edu>
2020-06-11 21:52:33 -0500

    calc popup adding scores

--- app/src/main/java/com/grapevineindustries/scoretracker/adapters/ComputeScoreRecyclerAdapter.kt
+++ app/src/main/java/com/grapevineindustries/scoretracker/adapters/ComputeScoreRecyclerAdapter.kt
@@ -1,5 +1,6 @@
 package com.grapevineindustries.scoretracker.adapters
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,13 +10,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.grapevineindustries.scoretracker.R
 import com.grapevineindustries.scoretracker.model.Player
+import kotlinx.android.synthetic.main.activity_popup_calc.*
+import kotlinx.android.synthetic.main.compute_score_list_item.*
+import kotlinx.android.synthetic.main.compute_score_list_item.view.*
+import kotlinx.android.synthetic.main.compute_score_list_item.view.btn_calcScore
 
 class ComputeScoreRecyclerAdapter(val context: Context, val players: ArrayList<Player>): RecyclerView.Adapter<ComputeScoreRecyclerAdapter.ComputeScoreHolder>() {
 
     inner class ComputeScoreHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val name: TextView? = itemView.findViewById(R.id.tv_name)
         val score: TextView? = itemView.findViewById(R.id.tv_score)
-        val addScore: EditText? = itemView.findViewById(R.id.et_score)
 
         fun bindComputeScore(player: Player, context: Context) {
             name?.text = player.name
@@ -25,6 +29,9 @@ class ComputeScoreRecyclerAdapter(val context: Context, val players: ArrayList<P
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComputeScoreHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.compute_score_list_item, parent, false)
+        view.btn_calcScore.setOnClickListener{
+            popupCalc(view)
+        }
         return ComputeScoreHolder(view)
     }
 
@@ -35,5 +42,72 @@ class ComputeScoreRecyclerAdapter(val context: Context, val players: ArrayList<P
     override fun onBindViewHolder(holder: ComputeScoreHolder, position: Int) {
         println("we got here")
         holder.bindComputeScore(players[position], context)
+    }
+
+    private fun popupCalc(view: View) {
+
+        val calcDialog = Dialog(context)
+        calcDialog.setContentView(R.layout.activity_popup_calc)
+        calcDialog.setTitle("Add Score:")
+
+        val btn3 = calcDialog.btn_3
+        val btn4 = calcDialog.btn_4
+        val btn5 = calcDialog.btn_5
+        val btn6 = calcDialog.btn_6
+        val btn7 = calcDialog.btn_7
+        val btn8 = calcDialog.btn_8
+        val btn9 = calcDialog.btn_9
+        val btn10 = calcDialog.btn_10
+        val btnJ = calcDialog.btn_j
+        val btnQ = calcDialog.btn_q
+        val btnK = calcDialog.btn_k
+        val btn20 = calcDialog.btn_20
+        val btn50 = calcDialog.btn_50
+        val btnDone = calcDialog.btn_done
+
+        btn3.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 3).toString()
+        }
+        btn4.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 4).toString()
+        }
+        btn5.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 5).toString()
+        }
+        btn6.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 6).toString()
+        }
+        btn7.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 7).toString()
+        }
+        btn8.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 8).toString()
+        }
+        btn9.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 9).toString()
+        }
+        btn10.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 10).toString()
+        }
+        btnJ.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 11).toString()
+        }
+        btnQ.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 12).toString()
+        }
+        btnK.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 13).toString()
+        }
+        btn20.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 20).toString()
+        }
+        btn50.setOnClickListener {
+            calcDialog.calcScore.text = (Integer.valueOf(calcDialog.calcScore.text.toString()) + 50).toString()
+        }
+        btnDone.setOnClickListener {
+            view.btn_calcScore.text = calcDialog.calcScore.text.toString()
+            calcDialog.dismiss()
+        }
+        calcDialog.show()
     }
 }
--- app/src/main/java/com/grapevineindustries/scoretracker/controller/ComputeScoreActivity.kt
+++ app/src/main/java/com/grapevineindustries/scoretracker/controller/ComputeScoreActivity.kt
@@ -1,6 +1,7 @@
 package com.grapevineindustries.scoretracker.controller
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,8 @@ import com.grapevineindustries.scoretracker.model.Player
 import com.grapevineindustries.scoretracker.utilities.EXTRA_NUM_PLAYERS
 import kotlinx.android.synthetic.main.activity_add_player.*
 import kotlinx.android.synthetic.main.activity_compute_score.*
+import kotlinx.android.synthetic.main.activity_popup_calc.*
+import kotlinx.android.synthetic.main.activity_popup_calc.view.*
 import kotlinx.android.synthetic.main.add_player_list_item.view.*
 import kotlinx.android.synthetic.main.compute_score_list_item.*
 import kotlinx.android.synthetic.main.compute_score_list_item.view.*
@@ -35,6 +38,10 @@ class ComputeScoreActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         rv_computeScore.layoutManager = layoutManager
         updateWildcard()
+
+
+
+
     }
 
     fun onTallyScoreClicked(view: View) {
@@ -44,17 +51,13 @@ class ComputeScoreActivity : AppCompatActivity() {
             val slot = rv_computeScore.getChildAt(num)
 
             Log.d("here__", num.toString())
-            var scoreToAdd = 0
-            if (slot.et_score.text.toString() != "")
-            {
-                scoreToAdd = Integer.valueOf(slot.et_score.text.toString())
-            }
+            var scoreToAdd = Integer.valueOf(slot.btn_calcScore.text.toString())
             println(playerList[num])
             playerList[num].score += scoreToAdd
             println(playerList[num])
 
             slot.tv_score.text = playerList[num].score.toString()
-            slot.et_score.text.clear()
+            slot.btn_calcScore.text = 0.toString()
         }
         updateWildcard()
 
@@ -63,6 +66,34 @@ class ComputeScoreActivity : AppCompatActivity() {
             val toast = Toast.makeText(this, "Game is over", Toast.LENGTH_SHORT)
             toast.show()
         }
+    }
+
+
+
+    private fun add(lhs: Int, rhs: Int): String {
+        return (lhs + rhs).toString()
+    }
+
+    fun clicked3(view: View) {
+        //val n = Integer.valueOf(view.calcScore.text.toString())
+        val n = 7
+        val value: String = when(view) {
+            view.btn_3 -> add(n, 3)
+            view.btn_4 -> add(n, 4)
+            view.btn_5 -> add(n, 5)
+            view.btn_6 -> add(n, 6)
+            view.btn_7 -> add(n, 7)
+            view.btn_8 -> add(n, 8)
+            view.btn_9 -> add(n, 9)
+            view.btn_10 -> add(n, 10)
+            view.btn_j -> add(n, 11)
+            view.btn_q -> add(n, 12)
+            view.btn_k -> add(n, 13)
+            view.btn_20 -> add(n, 20)
+            view.btn_50 -> add(n, 50)
+            else -> add(n, 0)
+        }
+        view.calcScore.text = value
     }
 
     fun convertWildcard(): String {
